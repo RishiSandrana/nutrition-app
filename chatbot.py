daf76cf2a0e2d4985bcbcc8a139db1e0e67398dd
RishiSandrana <rsandrana@cps.edu>
2024-02-20 15:14:25 -0600

    Initial app structure

--- chatbot.py
+++ chatbot.py
@@ -5,21 +5,24 @@ from vertexai.preview.generative_models import GenerativeModel
 project_id = "melodic-zoo-414700"
 location = "us-central1"
 
-vertexai.init(project = project_id, location=location)
+vertexai.init(project=project_id, location=location)
 model = GenerativeModel('gemini-pro')
 
 st.set_page_config(
-    page_title= "Nutrition Chatbot",
-    page_icon= "💬",
+    page_title="Nutrition Chatbot",
+    page_icon="💬",
 )
 
 st.title("Chat with Products!")
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 if prompt := st.chat_input("Type your question"):
     with st.chat_message("user"):
+        st.session_state.messages.append(prompt)
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        chat = model.start_chat(response_validation=False)
-        output = chat.send_message(prompt).text
-        response = st.write(output)
+        response = model.generate_content(st.session_state.messages)
+        st.markdown(response.text)
