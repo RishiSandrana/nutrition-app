cf082be0c01550cd87b3f0dc8c6a3dbf83792b22
RishiSandrana <rsandrana@cps.edu>
2024-02-27 00:42:34 -0600

    Integrated user preferences with LLM

--- preferences.py
+++ preferences.py
@@ -20,17 +20,26 @@ if "chat" not in st.session_state:
     st.session_state.chat = chat_model.start_chat()
 
 if "name" not in st.session_state:
-    st.session_state.name = ""
-
-st.session_state.name = st.text_input("What's your name?")
+    st.session_state.name = st.text_input("What's your name?")
+else:
+    st.session_state.name = st.text_input("What's your name?", value=st.session_state.name)
 
 if "age" not in st.session_state:
     st.session_state.age = st.slider('How old are you?', 1, 100)
 else:
     st.session_state.age = st.slider('How old are you?', 1, 100, st.session_state.age)
 
+if "allergies" not in st.session_state:
+    st.session_state.allergies = st.multiselect('Are you allergic to any of the following?',
+                                                ['Milk', 'Eggs', 'Fish', 'Crustacean shellfish', 'Tree nuts', 'Peanuts',
+                                                 'Wheat', 'Soybeans', 'Sesame'])
+else:
+    st.session_state.allergies = st.multiselect('Are you allergic to any of the following?',
+                                                ['Milk', 'Eggs', 'Fish', 'Crustacean shellfish', 'Tree nuts', 'Peanuts',
+                                                 'Wheat', 'Soybeans', 'Sesame'], default=st.session_state.allergies)
+
 button = st.button("Start chat!", type="primary")
 if button:
-    llm_context = f"You are designed to answer questions about food products. The person you're addressing is named {st.session_state.name}. They are {st.session_state.age} years old."
+    llm_context = f"You are designed to answer questions about food products. The person you're addressing is named {st.session_state.name}. They are {st.session_state.age} years old. They are allergic to {st.session_state.allergies}."
     st.session_state.chat = chat_model.start_chat(context=llm_context)
     switch_page('chatbot')
